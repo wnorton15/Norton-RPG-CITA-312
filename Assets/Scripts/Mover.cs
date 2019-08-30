1b805af72d0075849e0fd657ec9e376e47ca6b39
Will Norton <norton402@morrisville.edu>
2019-08-30 18:05:07 -0400

    click to move lecture 13

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,17 +7,24 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    Ray lastRay;
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            moveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    }
 
-        GetComponent<NavMeshAgent>().destination = target.position;
+    private void moveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
