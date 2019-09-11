a1d4088e0c32b6e13448bcad7c63ad69e787c06c
Will Norton <norton402@morrisville.edu>
2019-09-11 12:30:21 -0400

    Lecture 40

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -12,16 +12,17 @@ namespace RPG.Combat
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
 
-        Transform target;
+        Health target;
         float timeSinceLastAttack;
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
+            if (target.IsDead()) return;
 
             if (!GetIsInRange())
             {
-                GetComponent<Mover>().MoveTo(target.position);
+                GetComponent<Mover>().MoveTo(target.transform.position);
             }
             else
             {
@@ -45,24 +46,23 @@ namespace RPG.Combat
         //animation event 
         void Hit()
         {
-
-            Health healthComponent = target.GetComponent<Health>();
-            healthComponent.TakeDamage(weaponDamage);
+            target.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(gameObject.transform.position, target.position) < weaponRange;
+            return Vector3.Distance(gameObject.transform.position, target.transform.position) < weaponRange;
         }
 
         public void attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
         }
 
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack");
             target = null;
         }
 
