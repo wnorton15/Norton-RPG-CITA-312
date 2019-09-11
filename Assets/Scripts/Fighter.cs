53ae713f2ae6cbe384039c62a61a4b9a07315bf0
Will Norton <norton402@morrisville.edu>
2019-09-11 10:33:47 -0400

    Lecture 34

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -9,10 +9,13 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -29,7 +32,12 @@ namespace RPG.Combat
 
         private void AttackBehavior()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+            
         }
 
         private bool GetIsInRange()
