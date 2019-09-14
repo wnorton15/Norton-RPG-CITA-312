827cb7a5464fb4c5c19243de3616a454fc6f83db
Will Norton <norton402@morrisville.edu>
2019-09-14 12:41:28 -0400

    Lecture 45

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print(gameObject.name + " should chase");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
