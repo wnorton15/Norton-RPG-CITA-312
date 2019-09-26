33679ba425c33c99b8a593f30f1b626709cc6c4c
Will Norton <norton402@morrisville.edu>
2019-09-25 23:06:39 -0400

    Lecture 65

---
+++ Assets/Scripts/CinematicControlRemover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+
+        }
+    }
+}
