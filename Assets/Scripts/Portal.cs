d2ce94bce1fd032217ca599f928c2919f89ad255
Will Norton <norton402@morrisville.edu>
2019-09-29 21:29:53 -0400

    S7L71

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -13,8 +13,17 @@ namespace RPG.SceneManagement
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
         }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("scene loaded");
+            Destroy(gameObject);
+        }
+
     }
 }
