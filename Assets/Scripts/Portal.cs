94c850893980bf02338523125e4baed4a6543898
Will Norton <norton402@morrisville.edu>
2019-09-30 06:27:36 -0400

    S7L73

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -8,8 +8,15 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,6 +28,11 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
@@ -42,6 +54,10 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this)
+                {
+                    continue;
+                }
+                if (portal.destination != destination)
                 {
                     continue;
                 }
