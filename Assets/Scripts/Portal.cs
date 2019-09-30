66f2e590e4461af167f9abe1744f647747d4c55a
Will Norton <norton402@morrisville.edu>
2019-09-29 22:19:18 -0400

    S7L72

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,9 +23,31 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("scene loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
         }
 
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this)
+                {
+                    continue;
+                }
+                return portal;
+            }
+            return null;
+        }
     }
 }
