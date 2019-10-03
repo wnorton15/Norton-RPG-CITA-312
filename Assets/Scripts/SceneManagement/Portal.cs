3da72abb054911bd1e64bfbec3cd31869b739efb
Will Norton <norton402@morrisville.edu>
2019-10-02 22:36:48 -0400

    S8L83

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -56,6 +56,8 @@ namespace RPG.SceneManagement
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
 
+            wrapper.Save();
+
             yield return new WaitForSeconds(fadeWaitTime);
             yield return fader.FadeIn(fadeInTime);
 
--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -9,7 +9,17 @@ namespace RPG.SceneManagement
     public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
-        
+
+        [SerializeField] float fadeInTime = 1f;
+
+        private IEnumerator Start()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.L))
