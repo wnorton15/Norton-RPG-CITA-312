a13cb1d15d9bb3e1d82b1ad32551185fa3cca8a0
Will Norton <norton402@morrisville.edu>
2019-10-28 11:39:34 -0400

    S10L107

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
+using System;
 
 namespace RPG.Combat
 {
@@ -11,9 +12,17 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
+        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Transform handTransform = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
+
+
+        private void Start()
+        {
+            SpawnWeapon();
+        }
 
         private void Update()
         {
@@ -32,6 +41,13 @@ namespace RPG.Combat
             }
 
         }
+
+
+        private void SpawnWeapon()
+        {
+            Instantiate(weaponPrefab, handTransform);
+        }
+
 
         private void AttackBehavior()
         {
