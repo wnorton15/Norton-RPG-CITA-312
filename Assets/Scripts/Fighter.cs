3bb19909f911b4097792f4e83f88758f4c2d83b0
Will Norton <norton402@morrisville.edu>
2019-10-29 19:20:04 -0400

    S10L109

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -12,9 +12,11 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
-        [SerializeField] GameObject weaponPrefab = null;
+        
         [SerializeField] Transform handTransform = null;
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
+
+        [SerializeField] Weapon weapon = null;
+        
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
@@ -46,9 +48,12 @@ namespace RPG.Combat
 
         private void SpawnWeapon()
         {
-            Instantiate(weaponPrefab, handTransform);
+            if (weapon == null)
+            {
+                return;
+            }
             Animator animator = GetComponent<Animator>();
-            animator.runtimeAnimatorController = weaponOverride;
+            weapon.Spawn(handTransform, animator);
         }
 
 
