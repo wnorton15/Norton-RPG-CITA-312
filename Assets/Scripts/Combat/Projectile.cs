3f872c88b842b69d4a1aff37f3684f3156b2b281
Will Norton <norton402@morrisville.edu>
2019-11-05 20:03:23 -0500

    S10L118

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,18 +7,26 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1;
+    [SerializeField] bool isHoming = true;
     Health target = null;
     float damage = 0;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        transform.LookAt(GetAimLocation());
+    }
+    
     void Update()
     {
         if (target == null)
         {
             return;
         }
-
-        transform.LookAt(GetAimLocation());
+        if (isHoming && !target.IsDead())
+        {
+            transform.LookAt(GetAimLocation());
+        }
+        
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
@@ -41,6 +49,10 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != target)
+        {
+            return;
+        }
+        if (target.IsDead())
         {
             return;
         }
